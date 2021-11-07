daa0a7f0254a92bd416b9fa38fbaccddc0b46bed
koscew <koscew@gmail.com>
2021-11-06 22:18:56 -0700

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
-from sklearn.metrics.pairwise import cosine_distances
+from sklearn.metrics.pairwise import cosine_similarity
+#from sklearn.metrics.pairwise import cosine_distances
 import spacy
 nlp = spacy.load('en_core_web_sm')
 
